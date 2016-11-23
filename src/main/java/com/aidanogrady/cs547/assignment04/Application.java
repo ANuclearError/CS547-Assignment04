4e95cf305ebe841b41ba5b7b9da143a35c81900d
Aidan O'Grady <aidan@aidanogrady.com>
2016-11-23 09:42:36 +0000

    Added Maven project.

---
+++ src/main/java/com/aidanogrady/cs547/assignment04/Application.java
@@ -0,0 +1,14 @@
+package com.aidanogrady.cs547.assignment04;
+
+/**
+ * Main entry point.
+ *
+ * @author Aidan O'Grady
+ * @since 0.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("CS547 Assignment 04: Software Cost Estimation");
+        System.out.println("Author: Aidan O'Grady (201218150)\n");
+    }
+}
