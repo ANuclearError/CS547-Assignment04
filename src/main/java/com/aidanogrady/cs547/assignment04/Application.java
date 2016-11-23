433050cb831fb25a47029ed8c077410d3cd19e6b
Aidan O'Grady <aidan@aidanogrady.com>
2016-11-23 10:43:10 +0000

    Started work on file parsing.

--- src/main/java/com/aidanogrady/cs547/assignment04/Application.java
+++ src/main/java/com/aidanogrady/cs547/assignment04/Application.java
@@ -1,5 +1,12 @@
 package com.aidanogrady.cs547.assignment04;
 
+import com.aidanogrady.cs547.assignment04.data.DataParser;
+import com.aidanogrady.cs547.assignment04.data.DataRecord;
+import com.aidanogrady.cs547.assignment04.data.DataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Main entry point.
  *
@@ -10,5 +17,18 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("CS547 Assignment 04: Software Cost Estimation");
         System.out.println("Author: Aidan O'Grady (201218150)\n");
+
+        String fileName = "data/kemerer.arff";
+        List<String> attributes = new ArrayList<>();
+        attributes.add("KSLOC");
+        attributes.add("AdjFP");
+
+        DataSet dataSet = DataParser.parseFile(fileName, attributes);
+        for (DataRecord record : dataSet.getRecords()) {
+            for (String attribute : dataSet.getAttributes()) {
+                System.out.print(attribute + " " + record.get(attribute) + "\t");
+            }
+            System.out.println("\tEffort " + record.getEffort());
+        }
     }
 }
